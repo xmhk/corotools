567fdc183ab63a87621433c4f687eacd64599e4b
xmhk <chmhnk@googlemail.com>
2020-07-29 20:30:21 +0200

    extended example

--- example.py
+++ example.py
@@ -1,16 +1,21 @@
 import corotools as ct
 
-popfile = "example_data/worldpop.csv"
+popfilepath = "example_data/worldpop.csv"
 
 all_jhu_dicts = ct.get_jhu_data()
-population_dict = ct.get_populations(popfile)
+population_dict = ct.get_populations(popfilepath)
 
 # print stat for one country
 countrykey = 'Japan'
 
 print( ct.get_country_weekly_stat_table(all_jhu_dicts,population_dict, countrykey) )
 
-# print the metatable
-mt1 = ct.get_metatable124(all_jhu_dicts,population_dict,ct.list_europeancountries)
-print(mt1)
+# get the meta tables
+mt, mt2, mt4= ct.get_metatable124(all_jhu_dicts,population_dict,ct.list_europeancountries)
+
+# print example table
+mytable = ct.get_rank_table_from_df2(mt,mt2, mt4, 'cases', columns=['cases', 'cases_perpop'],
+                                     factors=[1.0/1000, 1000],
+                                     columnalias=['cases (k)','rel. cases (1/k)'])
+print(mytable)
 
